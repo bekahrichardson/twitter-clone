3895b0a53cbbddfdbf4ce3eaa9ef7206a6281b8f
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-02-04 10:29:54 -0700

    double the height

--- app/animations.js
+++ app/animations.js
@@ -4,19 +4,29 @@ $(document).ready(function() {
   $("tweet-controls").hide();
 
   $(".tweet-actions").hide();
+//	When the user clicks on the textarea, the textarea should double in size and the character count and Tweet buttons should be revealed.
 
-  
+$("textarea").on("click", function() {
+  var currentHeight = $(this).height();
+  var dblHeight = currentHeight * 2;
+  $(this).height(dblHeight);
+});
 
 
 
+
+
+
+$('.tweet-compose').val("");
+$('.tweet-compose').css("height", "2.5em");
+
+$('#char-count').text(140);
 
 
 });
 
 
 // Initially, the Tweet button and the character count button should be hidden (CSS).
-
-//	When the user clicks on the textarea, the textarea should double in size and the character count and Tweet buttons should be revealed.
 
 
 // As the user types, the character count should decrease.
