65c8b8f34d97416231947720f95fba617f2ba23f
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-02-04 10:59:31 -0700

    character count

--- app/animations.js
+++ app/animations.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-//hide tweet controls until user's in text area.
+// Initially, the Tweet button and the character count button should be hidden (CSS). (put display: none in the CSS to do this)
 
-  $("tweet-controls").hide();
+  $("#tweet-controls").hide();
 
   $(".tweet-actions").hide();
 //	When the user clicks on the textarea, the textarea should double in size and the character count and Tweet buttons should be revealed.
@@ -13,20 +13,30 @@ $("textarea").on("click", function() {
 });
 
 $("textarea").on("click", function() {
-  $("tweet-controls").show();
-  $("#char-count").show();
+  $(this).show("#tweet-controls");
+  $(this).show(".tweet-actions");
 });
-
-
-
-
-});
-
-
-// Initially, the Tweet button and the character count button should be hidden (CSS).
-
 
 // As the user types, the character count should decrease.
+$('textarea').keyup(function() {
+  var maxLength = 100;
+  var length = $(this).val().length;
+  var length = maxLength - length;
+  $('#char-count').text(length);
+})
+
+
+
+
+// When there are 10 or less characters, the character counter should turn red.
+
+
+
+});
+
+
+
+
 
 
 // When there are 10 or less characters, the character counter should turn red.
